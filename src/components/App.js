23a9fec91e27ee437b318fcc6082ba2f12973363
ihor <malishkoio@gmail.com>
2020-09-14 11:53:45 +0300

    splitting add lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Layout from './Layout';
 import Navigation from './Navigation';
-import HomePage from '../views/HomePage';
-import MoviePage from '../views/MoviesPage';
-import MovieDetailsPage from '../views/MovieDetailsPage';
 import NotFounde from '../views/NotFounde';
 import routes from '../routes';
+
+const HomePage = lazy(() => import('../views/HomePage'));
+const MoviePage = lazy(() => import('../views/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../views/MovieDetailsPage'));
 
 const App = () => (
   <Layout>
     <Navigation />
-
-    <Switch>
-      <Route path={routes.homePage} exact component={HomePage} />
-      <Route path={routes.movies} exact component={MoviePage} />
-      <Route path={routes.moviesDetails} component={MovieDetailsPage} />
-      <Route component={NotFounde} />
-    </Switch>
+    <Suspense fallback={<h2>Loader ...</h2>}>
+      <Switch>
+        <Route path={routes.homePage} exact component={HomePage} />
+        <Route path={routes.movies} exact component={MoviePage} />
+        <Route path={routes.moviesDetails} component={MovieDetailsPage} />
+        <Route component={NotFounde} />
+      </Switch>
+    </Suspense>
   </Layout>
 );
 
